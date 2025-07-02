0bc13cd8dea8cecc49b65427916e3632f8ac5ee9
Felipe Assis <169077824+felipeassiss@users.noreply.github.com>
2025-07-02 09:56:44 -0300

    scrapperytb.py

--- YTBscraper/scrapperytb.py
+++ YTBscraper/scrapperytb.py
@@ -1,8 +1,9 @@
 from googleapiclient.discovery import build
 import requests
+import csv
 
-#Key da Google Cloud - YouTube v3
-API_KEY = 'SUA CHAVE AQUI'
+#Key da Google Cloud - YouTube v3 ↓
+API_KEY = 'INSIRA SUA CHAVE AQUI!!'
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 def get_video_info(video_id):
@@ -27,16 +28,33 @@ def get_video_info(video_id):
     else:
         dislikes = 'N/A'
 
-    print("Título:", snippet['title'])
+    print("Título do vídeo:", snippet['title'])
     print("Canal:", snippet['channelTitle'])
-    print("Visualizações:", stats.get('viewCount', 'N/A'))
+    print("Views:", stats.get('viewCount', 'N/A'))
     print("Likes:", stats.get('likeCount', 'N/A'))
     print("Dislikes (estimado):", dislikes)
     print("Comentários:", stats.get('commentCount', 'N/A'))
 
+    return {
+        "Título do vídeo": snippet['title'],
+        "Canal": snippet['channelTitle'],
+        "Views": stats.get('viewCount', 'N/A'),
+        "Likes": stats.get('likeCount', 'N/A'),
+        "Dislikes (estimado)": dislikes,
+        "Comentários": stats.get('commentCount', 'N/A')
+    }
+
 if __name__ == '__main__':
-    #insira o ID presente na URL do vídeo no YouTube
+    #insira o ID presente na URL do vídeo no YouTube ↓
     video_id = 'LZGSuj4SkRg'
-    get_video_info(video_id)
+    info = get_video_info(video_id)
+
+    if info:
+        filename = 'youtube_video_info.csv'
+        with open(filename, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=info.keys())
+            writer.writeheader()
+            writer.writerow(info)
+        print(f'Arquivo "{filename}" criado com sucesso.')
 
 #feito para estudos por Felipe Assis.
